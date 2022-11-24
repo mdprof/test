19ce0bc200daafb4f4d3e9f9747e70089ce39608
mdprof <monada3d@yandex.ru>
2022-11-24 10:47:48 +0300

    После правок по ревью.
    1.
    Замечание:
    >>public static void getNumberOfPeople(Calculator calculator) {
    >>        Scanner sc = new Scanner(System.in);
    >>Сканнер лучше создать один раз и передавать в методы параметром.
    
    Решение:
    Scanner инициализируется один раз и передается в методы как параметр.
    
    2.
    Замечание:
    >>Calculator calculator = new Calculator("", 0);
    >>Можно не передавать это через конструктор, а инициализировать внутри или сразу при инициализации полей
    
    Решение:
    Начальные значения полей Calculator теперь присваиваются сразу в описании класса.
    
    3.
    Замечание:
    >>if (roundedPrice % 100 >= 11 && roundedPrice % 100 <= 19)
    >>на самом деле верхняя граница тут - 14, потому что остальные окончания - 15-16-17-18-19 будут такие же как у 5-6-7-8-9
    
    Решение:
    вместо if (roundedPrice % 100 >= 11 && roundedPrice % 100 <= 19)
    теперь if (roundedPrice % 100 >= 11 && roundedPrice % 100 <= 14)

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,12 @@
 public class Calculator {
-    int numberOfPeople;
-    String currentProduct;
-    double currentPrice;
-    String productList;
-    double allProductPrice;
-
-    Calculator(String productList, double allProductPrice) {
-        this.productList = productList;
-        this.allProductPrice = allProductPrice;
-    }
+    int numberOfPeople = 0;
+    String currentProduct = "";
+    double currentPrice = 0;
+    String productList = "";
+    double allProductPrice = 0;
 
     void addProductName(String str) {
-        this.productList +=str;
+        this.productList += str;
     }
 
     void priceSum(double price) {

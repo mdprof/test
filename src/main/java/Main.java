4fd2d92736218f39bf52f12b8669b9417de5d0ba
mdprof <monada3d@yandex.ru>
2022-12-12 10:15:04 +0300

    После правок по ревью.
    1.
    Замечание:
    >>public static void getNumberOfPeople(Calculator calculator) {
    >>        Scanner sc = new Scanner(System.in);
    >>Сканнер лучше создать один раз и передавать в методы параметром.
    
    Решение:
    Scanner инициализируется один раз и передается в методы как параметр.
    
    2.
    Замечание:
    >>Calculator calculator = new Calculator("", 0);
    >>Можно не передавать это через конструктор, а инициализировать внутри или сразу при инициализации полей
    
    Решение:
    Начальные значения полей Calculator теперь присваиваются сразу в описании класса.
    
    3.
    Замечание:
    >>if (roundedPrice % 100 >= 11 && roundedPrice % 100 <= 19)
    >>на самом деле верхняя граница тут - 14, потому что остальные окончания - 15-16-17-18-19 будут такие же как у 5-6-7-8-9
    
    Решение:
    вместо if (roundedPrice % 100 >= 11 && roundedPrice % 100 <= 19)
    теперь if (roundedPrice % 100 >= 11 && roundedPrice % 100 <= 14)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,101 +1,29 @@
-import java.util.Scanner;
-111
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] params) {
 
-        Calculator calculator = new Calculator("", 0);
+        HashSet<String> mySet = new HashSet<>();
+        mySet.add("qqq");
+        mySet.add("www");
+        mySet.add("eee");
+        mySet.add("rrr");
 
-        //Запрашиваем количество людей и помещаем в calculator.numberOfPeople.
-        System.out.print("На сколько человек разделить счет? ");
-        getNumberOfPeople(calculator);
+        Iterator iterator = mySet.iterator();
 
-        //Выполняем основной цикл пока isExit() == false
-        do {
-            System.out.println("Введите название нового товара:");
-            addNewProductName(calculator);
-            System.out.println("Введите цену товара в формате \"рубли,копейки\"");
-            addNewProductPrice(calculator);
-            System.out.printf("Вы добавили товар: %s с ценой %.2f %s\n", calculator.currentProduct, calculator.currentPrice, wordRoubleEnding(calculator.currentPrice));
-        } while (!isExit());
-
-        //Выводим результат
-        printResult(calculator);
-    }
-
-    //Метод getNumberOfPeople() запрашивает ввод с консоли значения и помещает его в calculator.numberOfPeople,
-    // если значение меньше 1 или не является целым числом, то запрашивает повторно.
-    public static void getNumberOfPeople(Calculator calculator) {
-        Scanner sc = new Scanner(System.in);
-        do {
-            System.out.println("Введите целое число больше 1.");
-            while (!sc.hasNextInt()) {
-                System.out.println("Введите целое число!");
-                sc.next();
-            }
-            calculator.numberOfPeople = sc.nextInt();
-        } while (calculator.numberOfPeople <= 1);
-    }
-
-    // Метод addNewProductName() запрашивает ввод названия товара и добавляет его в калькулятор
-    public static void addNewProductName(Calculator calculator) {
-        Scanner sc = new Scanner(System.in);
-        //Запрашиваем название товара "
-        calculator.currentProduct = sc.next();
-        //Добавляем название товара в калькулятор
-        calculator.addProductName(calculator.currentProduct + "\n");
-    }
-
-    //Метод addNewProductPrice() запрашивает цену товара и добавляет ее в калькулятор.
-    public static void addNewProductPrice(Calculator calculator) {
-        Scanner sc = new Scanner(System.in);
-        //Запрашиваем цену товара в нужном формате, если формат неверный - выводим ошибку и запрашиваем повторно.
-        while (!sc.hasNextDouble()) {
-            System.out.println("Неверный формат цены!\nВведите цену товара в формате \"рубли,копейки\"");
-            sc.next();
+        System.out.println(mySet);
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
         }
-        //Если формат цены верный, то присваиваем цену calculator.currentPrice и суммируем в калькуляторе.
-        calculator.currentPrice = sc.nextDouble();
-        calculator.priceSum(calculator.currentPrice);
-    }
 
-    //Метод isExit() возвращает true, если ввели "завершить" иначе возвражает false
-    public static boolean isExit() {
-        Scanner sc = new Scanner(System.in);
-        //Запрашиваем надо ли добавить новый товар или завершить.
-        System.out.println("Для добавления нового товара введите любой символ, для завершения введите \"завершить\".");
-        //Если ввели "завершить", то возвращаем true иначе возвращаем false
-        return sc.next().equalsIgnoreCase("завершить");
-    }
+        for (String str: mySet) {
+            System.out.println(str);
+            //mySet.remove(str);
 
-    //Метод printResult() выводит список товаров, цену на каждого человека, и слово "рубль" с нужным окончанием
-    public static void printResult(Calculator calculator) {
-        double pricePerPerson = calculator.allProductPrice / calculator.numberOfPeople;
-        System.out.print("Добавленные товары:\n" + calculator.productList);
-        System.out.printf("Каждый должен заплатить: %.2f %s.\n", pricePerPerson, wordRoubleEnding(pricePerPerson));
-    }
-
-    //Метод wordEnding() возвращает строку с нужным написанием слова "рубль" в зависимости от последних цифр целой части цены.
-    public static String wordRoubleEnding(double price) {
-        int roundedPrice = (int) Math.floor(price);
-        String roubleEnding;
-        if (roundedPrice % 100 >= 11 && roundedPrice % 100 <= 19) {
-            roubleEnding = "рублей";
-        } else {
-            switch (roundedPrice % 10) {
-                case 1:
-                    roubleEnding = "рубль";
-                    break;
-                case 2:
-                case 3:
-                case 4:
-                    roubleEnding = "рубля";
-                    break;
-                default:
-                    roubleEnding = "рублей";
-            }
         }
-        return roubleEnding;
     }
+
+
 }
-
